045b1123757961caa9f2f2fa378f64d4b6bb13c0
Kingsley <kingsley550@yahoo.com>
2022-10-10 23:39:02 -0500

    Establish sign up route

--- chatsley/main_app/views.py
+++ chatsley/main_app/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login 
+from django.shortcuts import render,redirect
+from .forms import SignUp
 
 # Create your views here.
 def homepage(request):
     return render(request,'homepage.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUp(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('homepage')
+    else: 
+        form = SignUp()
+
+    return render(request, 'signup.html', {'form':form})
