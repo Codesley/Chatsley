25e8b2ac329f200a723856a39fb2aa6810d55b72
Kingsley <kingsley550@yahoo.com>
2022-10-14 15:36:54 -0500

    MVP

--- chatsley/chatrooms/consumers.py
+++ chatsley/chatrooms/consumers.py
@@ -1,7 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
-
+from django.contrib.auth.models import User
+from .models import Message, Room
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -25,6 +26,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         username = data['username']
         room = data['room']
 
+        await self.save_message(username, room, message)
+
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -45,3 +48,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'username' : username,
             'room': room, 
         }))
+
+    @sync_to_async
+    def save_message(self, username, room, message):
+        user = User.objects.get(username=username)
+        room = Room.objects.get(slug=room)
+
+        Message.objects.create(user=user, room=room, content=message)
